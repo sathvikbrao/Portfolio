b66f96e99d433b3674e0494bbe272661d1d730c6
Sathvik B Rao <sathvikbrao2002@gmail.com>
2023-05-03 16:20:52 +0530

    Added Files

---
+++ index.js
@@ -0,0 +1,42 @@
+
+function scrollToContact() {
+  var contactMeSection = document.getElementById("Contactme");
+  contactMeSection.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToAbout() {
+  var contactMeSection = document.getElementById("AboutMe");
+  contactMeSection.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToCertifications() {
+  var contactMeSection = document.getElementById("Certifications");
+  contactMeSection.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToTechStack() {
+  var contactMeSection = document.getElementById("TechStack");
+  contactMeSection.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToProjects() {
+  var contactMeSection = document.getElementById("Projects");
+  contactMeSection.scrollIntoView({ behavior: "smooth" });
+}
+
+function setEmpty(){
+  var userName = document.querySelector(".name");
+  var userEmail = document.querySelector(".email");
+  var userMessage = document.querySelector(".message");
+  
+  userName.value = "";
+  userName.placeholder = "Name";
+  
+  userEmail.value = "";
+  userEmail.placeholder = "Email";
+  
+  userMessage.value = "";
+  userMessage.placeholder = "Message";
+
+}
+
